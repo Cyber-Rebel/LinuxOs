3cc2ef744c6923059fa3ad8466d6afca6a9e1687
Cyber-Rebel <np103177@gmail.com>
2025-06-28 19:37:35 +0530

    Folder Text Fixed

--- src/components/Folderrr.jsx
+++ src/components/Folderrr.jsx
@@ -58,7 +58,7 @@ const folders = [
       </div>
 
 
-     {Folderss.length < 0 ? (
+     {Folderss.length === 0 ? (
       <div className="text-white text-3xl  ">First Create A Folder By Right Click</div>
 ) : (
   <div className="grid grid-cols-6 gap-4">
